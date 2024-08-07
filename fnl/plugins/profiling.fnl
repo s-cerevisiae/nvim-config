33b950043d44efc940ba4842ba3bd0af3b09e3a6
spore <spore2050@gmail.com>
2024-08-07 17:57:45 +0800

    remove `vim-startuptime`

--- fnl/plugins/profiling.fnl
+++
@@ -1,2 +0,0 @@
-[{1 "dstein64/vim-startuptime"
-  :cmd "StartupTime"}]
--- lua/plugins/profiling.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/profiling.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"dstein64/vim-startuptime", cmd = "StartupTime"}}
